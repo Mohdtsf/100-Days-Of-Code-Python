5f8c70af0d22f0997ae75f2cb51c79035fe3e4da
Mohd Tauseef Ansari <97387059+Mohdtsf@users.noreply.github.com>
2022-12-20 10:14:21 +0000

    #Day-3-2nd Exercise Created a BMI 2.0 Calculator using if, elif, and else logical operators

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # 🚨 Don't change the code below 👇
-number = int(input("Which number do you want to check? "))
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-
-if number % 2 == 0:
-  print("This is an even number")
+bmi = round(weight/height**2)
+if bmi < 18.5:
+  print(f"Your bmi is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your bmi is {bmi}, you are normal weigth.")
+elif bmi < 30:
+  print(f"Your bmi is {bmi}, you are overweight.")
+elif bmi < 35:
+  print(f"Your bmi is {bmi}, you are obese.")
 else:
-  print("This ia an odd number")
+  print(f"Your bmi is {bmi}, you are clinically obese.")
 
 
